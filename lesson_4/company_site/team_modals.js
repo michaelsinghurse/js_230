9c49a83a5eee1453c15ecc3f4a7ff296a92df538
Michael Singhurse <michaelsinghurse@gmail.com>
2020-06-08 16:38:53 -0500

    finished exercises. my modifications are in team.html site.css and team_modals.js

---
+++ lesson_4/company_site/team_modals.js
@@ -0,0 +1,43 @@
+$(function() {
+  let $modals = $(".modal");
+  let $overlays = $(".overlay");
+  let FADE_DURATION = 600;
+
+  $("#team li > a").click(function(event) {
+    console.log("anchor click");
+    event.preventDefault();
+
+    let $overlay = $(event.target).closest("a").siblings(".overlay");
+    $overlay.fadeToggle(FADE_DURATION); 
+    
+    let $modal = $overlay.children(".modal");
+    $modal.fadeToggle(FADE_DURATION); 
+  });
+  
+  $overlays.click(function(event) {
+    console.log("overlay click...");
+    $(this).fadeToggle(FADE_DURATION);
+    $(this).children(".modal").fadeToggle(FADE_DURATION);
+  });
+
+  $modals.click(function(event) {
+    console.log("modal click...");
+
+    event.stopPropagation();
+    let $modal = $(this);
+    if (event.target === $modal.children("a").get(0)) {
+      event.preventDefault();
+      $modal.fadeToggle(FADE_DURATION);
+      $modal.parent(".overlay").fadeToggle(FADE_DURATION);
+    }
+  });
+
+  document.addEventListener("keyup", function(event) {
+    if (event.key === "Escape") {
+      let $overlay = $overlays.filter((_, element) => {
+        return $(element).css("display") !== "none"; 
+      });
+      $overlay.trigger("click");
+    }
+  });
+});
