f2e5ee3a9abc6553e9794e3d6b21f49cf3b125f2
Michael Singhurse <michaelsinghurse@gmail.com>
2020-06-17 16:31:09 -0500

    html and css are complete. have yet to start on the javascript

---
+++ exercises/misc_apps/animals/app.js
@@ -0,0 +1,9 @@
+$(function() {
+  // add listener on all the images
+  // - hover for 2 seconds
+  // - select the figcaption sibling
+  // - toggle it's display property (current its display: none)
+  // add listener for mouseleave
+  // - if the figcaption is visible, turn off the display
+
+});
