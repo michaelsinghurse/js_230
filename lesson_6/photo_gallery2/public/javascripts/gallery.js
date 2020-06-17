79ce4de112f5d3efe4bf39b189aeacb874c05e8a
Michael Singhurse <michaelsinghurse@gmail.com>
2020-06-17 14:53:24 -0500

    completed exercise

--- lesson_6/photo_gallery2/public/javascripts/gallery.js
+++ lesson_6/photo_gallery2/public/javascripts/gallery.js
@@ -5,26 +5,189 @@ $(function() {
     let $template = $(element);
     $template.remove();
     templates[$template.attr("id")] = Handlebars.compile($template.html());
+
+    if ($template.data("type") === "partial") {
+      Handlebars.registerPartial(
+        $template.attr("id").replace("photo_", ""),
+        $template.html());
+    }
   });
   
-  function renderPhotoSlides(photos) {
-    $("#slides").append(templates["photos"]({ photos }));
-  }
+  let slideshow = {
+    DURATION: 500,
+    photos: [],
+    $slideshow: $("#slideshow"),
 
-  function renderPhotoInformation(photo) {
-    $("section > header").append(templates["photo_information"](photo));
-  }
+    bind: function() {
+      this.$slideshow.find("ul").on("click", "a", this.toggleSlide.bind(this));
+      $("section > header").on("click", ".actions a", this.likeOrFavoritePhoto.bind(this));
+      $("#comments > form").on("submit", this.formSubmit.bind(this));
+    },
+    
+    favoritePhoto: function(id) {
+      $.ajax({
+        url: "photos/favorite",
+        method: "POST",
+        data: "photo_id=" + id,
+        dataType: "json",
+      })
+        .done(function(data, textStatus) {
+          if (textStatus === "success") {
+            let index = this.photos.findIndex(photo => photo.id === Number(id));
+            this.photos[index].favorites = data.total;
+            this.renderPhotoInformation(index);
+          }
+        }.bind(this))
+        .fail(function() {
+          console.log("Failure favoriting photo!");
+        });
+    },
 
-  $.ajax({
-    url: "/photos",
-    method: "GET",
-    dataType: "json",
-  })
-    .done(function(data) {
-      renderPhotoSlides(data);
-      renderPhotoInformation(data[0]);
-    })
-    .fail(function() {
-      console.log("Fail!");
-    });
+    formSubmit: function(event) {
+      event.preventDefault();
+      let $form = $(event.currentTarget);
+
+      // manually update id on form to currently visible photo id
+      let id = this.$slideshow.find("figure:visible").attr("data-id");
+      $form.find("[name='photo_id']").val(id);
+
+      $.ajax({
+        url: $form.attr("action"),
+        method: $form.attr("method"), 
+        data: $form.serialize(), 
+      })
+        .done(function(data, textStatus) {
+          if (textStatus === "success") {
+            $("#comments > ul").append(templates["photo_comment"](data));      
+            $form.get(0).reset(); 
+          }
+        }.bind(this))
+        .fail(function() {
+          console.log("Failure submitting comment!");
+        });
+
+    },
+
+    handleComments: function(comments) {
+      this.renderComments(comments);
+    },
+    
+    handlePhotos: function() {
+      this.renderPhotos();
+      this.renderPhotoInformation(0);
+      this.loadComments(0);
+    },
+  
+    init: function() {
+      this.loadPhotos();
+      this.bind();
+    },
+    
+    likeOrFavoritePhoto: function(event) {
+      event.preventDefault();
+
+      if (event.target.classList.contains("like")) {
+        this.likePhoto(event.target.getAttribute("data-id"));
+      } else {
+        this.favoritePhoto(event.target.getAttribute("data-id"));
+      }
+    },
+
+    likePhoto: function(id) {
+      $.ajax({
+        url: "photos/like",
+        method: "POST",
+        data: "photo_id=" + id,
+        dataType: "json",
+      })
+        .done(function(data, textStatus) {
+          if (textStatus === "success") {
+            let index = this.photos.findIndex(photo => photo.id === Number(id));
+            this.photos[index].likes = data.total;
+            this.renderPhotoInformation(index);
+          }
+        }.bind(this))
+        .fail(function() {
+          console.log("Failure liking photo!");
+        });
+    },
+
+    loadComments: function(index) {
+      let id = this.photos[index].id; 
+
+      $.ajax({
+        url: "/comments?photo_id=" + id,
+        method: "GET",
+        dataType: "json",
+      })
+        .done(function(data, textStatus) {
+          if (textStatus === "success") {
+            this.handleComments(data);
+          }
+        }.bind(this))
+        .fail(function() {
+          console.log("Failure loading comments!");
+        });
+    },
+  
+    loadPhotos: function() {
+      $.ajax({
+        url: "/photos",
+        method: "GET",
+        dataType: "json",
+      })
+        .done(function(data, textStatus) {
+          if (textStatus === "success") { 
+            this.photos = data;
+            this.handlePhotos();
+          }
+        }.bind(this))
+        .fail(function() {
+          console.log("Failure loading photos!");
+        });
+    },
+
+    renderComments: function(comments) {
+      $("#comments > ul").html(templates["photo_comments"]({ comments }));
+    },
+
+    renderPhotos: function() {
+      this.$slideshow.find("#slides").html(
+        templates["photos"]({ photos: this.photos }));
+    },
+
+    renderPhotoContent: function(index) {
+      this.renderPhotoInformation(index);
+      this.loadComments(index);
+    },
+
+    renderPhotoInformation: function(index) {
+      $("section > header").html(templates["photo_information"](this.photos[index]));
+    },
+  
+    toggleSlide: function(event) {
+      event.preventDefault();
+
+      let currentIndex = this.$slideshow.find("figure:visible").index();
+      
+      let newIndex = event.target.classList.contains("next")
+        ? currentIndex + 1
+        : currentIndex - 1;
+      
+      if (newIndex < 0) {
+        newIndex = this.photos.length - 1;
+      } else if (newIndex > this.photos.length - 1) {
+        newIndex = 0;
+      }
+      
+      this.$slideshow.find("figure").eq(currentIndex).fadeOut(this.DURATION);
+      this.$slideshow.find("figure").eq(newIndex).fadeIn(this.DURATION);
+
+      this.renderPhotoContent(newIndex);
+    },
+  };
+
+  slideshow.init();
 });
+
+
