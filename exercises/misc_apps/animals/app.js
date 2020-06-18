ef8f29dc78dda62351b825f28b7f361eaaf47102
Michael Singhurse <michaelsinghurse@gmail.com>
2020-06-18 10:40:16 -0500

    complete exercise

--- exercises/misc_apps/animals/app.js
+++ exercises/misc_apps/animals/app.js
@@ -1,9 +1,19 @@
 $(function() {
-  // add listener on all the images
-  // - hover for 2 seconds
-  // - select the figcaption sibling
-  // - toggle it's display property (current its display: none)
-  // add listener for mouseleave
-  // - if the figcaption is visible, turn off the display
+  const WAIT = 2 * 1000;
+  let timeoutId;
 
+  let $animals = $("#animals");
+  
+  function toggleTooltip(img, visible) {
+    $(img).next().toggle(visible);
+  }
+
+  $animals.on("mouseover", "img", function(event) {
+    timeoutId = setTimeout(() => toggleTooltip(this, true), WAIT);
+  });
+
+  $animals.on("mouseleave", "img", function(event) {
+    clearTimeout(timeoutId);
+    toggleTooltip(this, false);
+  });
 });
