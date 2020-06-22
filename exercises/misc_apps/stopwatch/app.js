351f6568c24c54f66d4646642e7816907b826d57
Michael Singhurse <michaelsinghurse@gmail.com>
2020-06-22 10:15:23 -0500

    timer is now working. changed font size on clock because only rendering three units of time rather than all four

--- exercises/misc_apps/stopwatch/app.js
+++ exercises/misc_apps/stopwatch/app.js
@@ -10,31 +10,69 @@ let stopwatch = {
   },
   
   handleResetClick(event) {
-    console.log("reset button clicked!");
+    this.time = 0;
+    this.renderTime();
   },
 
   handleStartStopClick(event) {
     if (event.target.className ==="start") {
-      console.log("start clicked!");
       event.target.className = "stop";
+      event.target.textContent = "Stop";
+      this.startTimer();
     } else {
-      console.log("stop clicked!");
       event.target.className = "start";
+      event.target.textContent = "Start";
+      this.stopTimer();
     }
   },
+  
+  incrementTime() {
+    this.time += 1;
+  },
 
-  init() {
-    this.time = 0;
+  init(initialTime = 0) {
+    this.time = initialTime;
     this.intervalId = null;
     this.bindEventListeners();
     this.renderTime();
   },
+  
+  pad(num) {
+    let numString = String(num);
+    return numString.length === 1 ? "0" + numString : numString;
+  },
 
   renderTime() {
-    document.getElementById("clock").textContent = "01:23:45:67";
-  }
+    let hrs = Math.floor(this.time / 360000);
+    let remainder = this.time % 360000;
+    let min = Math.floor(remainder / 6000);
+    remainder = remainder % 6000;
+    let sec = Math.floor(remainder / 100);
+    let cs = remainder % 100;
+    
+    let time;
+
+    if (hrs > 0) {
+      time = this.pad(hrs) + ":" + this.pad(min) + ":" + this.pad(sec);
+    } else {
+      time = this.pad(min) + ":" + this.pad(sec) + ":" + this.pad(cs);
+    }
+
+    document.getElementById("clock").textContent = time;
+  },
+
+  startTimer() {
+    this.intervalId = setInterval(() => {
+      this.incrementTime();
+      this.renderTime();
+    }, 10);
+  },
+
+  stopTimer() {
+    clearInterval(this.intervalId);
+  },
 };
 
 document.addEventListener("DOMContentLoaded", _unusedEvent => {
-  stopwatch.init();
+  stopwatch.init(359000);
 });
