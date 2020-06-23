b5639af609f5c6dd219747916a367aa3ed3adf8e
Michael Singhurse <michaelsinghurse@gmail.com>
2020-06-23 11:42:17 -0500

    completed exercise

--- exercises/misc_apps/text_editor/app.js
+++ exercises/misc_apps/text_editor/app.js
@@ -1,5 +1,52 @@
-$(function() {
-  $("button").on("click", function(event) {
-    $(this).toggleClass("enabled");
-  });
-});
+let Editor = {
+  bindEvents() {
+    $("#menu").on("click", "button", this.handleButtonClick.bind(this)); 
+  },
+  
+  defaultButtonStates() {
+    $("[data-command='justifyLeft']").addClass("enabled");
+  },
+
+  formatText(commandName) {
+    let showDefaultUI = false;
+    let valueArgument = null;
+    
+    if (commandName === "createLink") {
+      valueArgument = prompt("Please enter a url:", "http://example.com");
+      $("[data-command='createLink']").removeClass("enabled");
+    }
+
+    document.execCommand(commandName, showDefaultUI, valueArgument);
+  },
+
+  handleButtonClick(event) {
+    let $button = $(event.target.closest("button"));
+    $button.toggleClass("enabled");
+
+    let command = $button.attr("data-command");
+    if (command.includes("justify")) {
+      this.toggleJustifyButtons(command);
+    }
+
+    this.formatText(command);     
+    $("#content").trigger("focus");
+  },
+  
+  init() {
+    this.defaultButtonStates();
+    this.bindEvents();
+  },
+
+  toggleJustifyButtons(command) {
+    let $buttons = $("[data-command^='justify']");
+    $buttons.each((_, element) => {
+      let $button = $(element);
+      if ($button.attr("data-command") !== command) {
+        $button.removeClass("enabled");
+      }
+    });
+  },
+
+};
+
+$(Editor.init.bind(Editor));
